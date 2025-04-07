8f92dd949ea2465491fb6f107b0c035eaac64897
Zoey Hamm <339622581@GoTVDSB.ca>
2025-04-07 13:36:17 -0400

    Started tutorial

--- Game1.cs
+++ Game1.cs
@@ -18,16 +18,12 @@ namespace Monogame_2._5___Moving_in_Waves_and_Circles
 
         protected override void Initialize()
         {
-            // TODO: Add your initialization logic here
-
             base.Initialize();
         }
 
         protected override void LoadContent()
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
-
-            // TODO: use this.Content to load your game content here
         }
 
         protected override void Update(GameTime gameTime)
@@ -35,16 +31,12 @@ namespace Monogame_2._5___Moving_in_Waves_and_Circles
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
-            // TODO: Add your update logic here
-
             base.Update(gameTime);
         }
 
         protected override void Draw(GameTime gameTime)
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
-
-            // TODO: Add your drawing code here
 
             base.Draw(gameTime);
         }
